25bcb2538e070de47d654c404355fc950c49fee4
Théo Reichel <theo@nolog.ch>
2022-02-15 15:50:56 +0100

    Fix tests for review

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,24 +3,20 @@ require 'rails_helper'
 RSpec.describe Message, :type => :model do
   it "should validate presence of content" do
     message = FactoryBot.build(:message, content: nil)
-    expect { message.save! }.to raise_error(
-                                ActiveRecord::RecordInvalid
-                              )
+    expect(message.valid?).to be(false)
+    expect(message.errors.messages).to include(:content)
   end
 
   it "should validate presence of user" do
     message = FactoryBot.build(:message, user: nil)
-    expect { message.save! }.to raise_error(
-                                  ActiveRecord::RecordInvalid
-                                )
+    expect(message.valid?).to be(false)
+    expect(message.errors.messages).to include(:user)
   end
 
   it "should validate creation of message" do
     user = User.new(username: "username123")
     message = Message.new(content: "Lorem ipsum", user: user)
-
-    expect(message.content).to eql("Lorem ipsum")
-    expect(message.user).to eql(user)
+    expect(message).to be_valid
   end
 end
 
